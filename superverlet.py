f49bdf0d7378e37e1c506b95a0cfefda879d1c45
dotargz <chamberlainsal@gmail.com>
2022-11-06 02:28:19 -0800

    Decrease usage of assests and optimize

--- superverlet.py
+++ superverlet.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import gfxdraw
 import sys
 import argparse
 import uuid
@@ -19,9 +20,15 @@ parser.add_argument("-g", "--gravity", type=float, default=9.81,
                     help="Amount of gravity applied to the each object")
 parser.add_argument("-s", "--sound", action="store_true",
                     help="Enable sound")
+parser.add_argument("-r", "--radius", type=int, default=16,
+                    help="Radius of all the circles")
+parser.add_argument("-t", "--deltatime", type=float, default=(1/60),
+                    help="Delta time for the simulation")
+parser.add_argument("--use-experimental-rendering", action="store_true",
+                    help="Use experimental rendering")
 ARGS = parser.parse_args()
 print("Launching SuperVerlet with the following arguments:")
-for k,v in parser.parse_args().__dict__.items():
+for k, v in parser.parse_args().__dict__.items():
     print('%s: %s' % (k, v))
 
 pygame.init()
@@ -42,8 +49,13 @@ def resource_path(relative_path):
 DISPLAYSURF = pygame.display.set_mode(
     (ARGS.width, ARGS.height), pygame.RESIZABLE)
 SCREEN_WIDTH, SCREEN_HEIGHT = DISPLAYSURF.get_size()
-logo = pygame.image.load(resource_path("assets/img/logo.png"))
-pygame.display.set_icon(logo)
+
+try:
+    logo = pygame.image.load(resource_path("assets/img/logo.png"))
+    pygame.display.set_icon(logo)
+except:
+    pass
+
 pygame.display.set_caption("SuperVerlet v1.2.1 (beta)")
 
 # Initial FPS setup
@@ -56,7 +68,12 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-FONT = pygame.font.Font(resource_path('assets/fonts/UbuntuMono-Bold.ttf'), 32)
+
+try:
+    FONT = pygame.font.Font(resource_path('assets/fonts/UbuntuMono-Bold.ttf'), 32)
+except:
+    FONT = pygame.font.SysFont('Monospace', 32)
+
 DEBUG = ARGS.debug
 
 # UI Setup
@@ -76,15 +93,15 @@ dtCounterRect = dtCounter.get_rect()
 dtCounterRect.center = ((dtCounterRect.width/2)+4,
                         (dtCounterRect.height/2)+fpsCounterRect.height+ObjcounterRect.height)
 
-# Cursor setup
-cursor = pygame.sprite.Sprite()
-cursor.image = pygame.image.load(resource_path("assets/img/cursor.png"))
-cursor.rect = cursor.image.get_rect()
-cursor.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-
 
 # Sound Setup
-spawn_sound = pygame.mixer.Sound(resource_path('assets/sounds/spawn.wav'))
+try:
+    if ARGS.sound:
+        spawn_sound = pygame.mixer.Sound(resource_path('assets/sounds/spawn.wav'))
+        spawn_sound.set_volume(0.05)
+except:
+    pass
+
 
 
 # Function setup
@@ -94,16 +111,22 @@ def totuple(a):
     except TypeError:
         return a
 
+
+def draw_circle(surface, x, y, radius, color):
+    if ARGS.use_experimental_rendering:
+        gfxdraw.aacircle(surface, int(x), int(y), int(radius), color)
+        gfxdraw.filled_circle(surface, int(x), int(y), int(radius), color)
+    else:
+        pygame.draw.circle(surface, color, (x, y), radius)
+
 # Circle (Physics object)
 
 
 class Circle(pygame.sprite.Sprite):
     def __init__(self, starting_pos=[SCREEN_WIDTH/2, SCREEN_HEIGHT/2]):
         super().__init__()
-        self.image = pygame.image.load(resource_path("assets/img/circle.png"))
-        self.rect = self.image.get_rect()
+        self.radius = ARGS.radius
         self.starting_pos = starting_pos
-        self.rect.center = totuple(starting_pos)
         self.position_current = np.array(starting_pos)
         self.position_old = np.array(starting_pos)
         self.acceleration = np.array(starting_pos)
@@ -116,8 +139,6 @@ class Circle(pygame.sprite.Sprite):
         self.position_current = self.position_current + \
             velocity + self.acceleration * dt * dt
 
-        self.rect.center = totuple(self.position_current)
-
         # reset acceleration
         self.acceleration = np.array([0, 0])
 
@@ -125,12 +146,12 @@ class Circle(pygame.sprite.Sprite):
         self.acceleration = self.acceleration + acc
 
     def draw(self, surface):
-        surface.blit(self.image, self.rect)
+        draw_circle(surface, self.position_current[0], self.position_current[1], self.radius, WHITE)
         if DEBUG:
-            self.drawvelocity(surface, 2, RED)
+            self.drawvelocity(surface, 3, RED)
 
     def drawvelocity(self, surface, multiplier=1, color=RED):
-        pygame.draw.line(surface, color, self.rect.center, totuple(
+        pygame.draw.line(surface, color, self.position_current, totuple(
             self.position_current + (self.position_current*multiplier - self.position_old*multiplier)), 2)
 
 # Physics Solver
@@ -167,11 +188,12 @@ class Solver():
             to_obj = obj.position_current - position
             dist = np.sqrt((to_obj[0]*to_obj[0])+(to_obj[1]*to_obj[1]))
             # 32 is default radius
-            if (dist > radius - 16):
+            if (dist > radius - ARGS.radius):
                 n = to_obj / dist
-                obj.position_current = position + n * (radius - 16)
+                obj.position_current = position + n * (radius - ARGS.radius)
 
-    # very inefficient collision detection
+    # very inefficient collision detection, but it works
+    # we also add a small offset to the radius to prevent objects from overlapping a little when using experimental rendering
     def solveCollisions(self):
         object_container = self.getInstances()
         object_count = len(object_container)
@@ -181,16 +203,21 @@ class Solver():
                     object_container[k].position_current
                 dist = np.sqrt(
                     (collision_axis[0]*collision_axis[0])+(collision_axis[1]*collision_axis[1]))
-                if dist < 32:
+                if dist < ARGS.radius*2+ERO:
                     n = collision_axis / dist
-                    delta = 32 - dist
+                    delta = ARGS.radius*2+ERO - dist
                     object_container[i].position_current = object_container[i].position_current + 0.25 * delta * n
                     object_container[k].position_current = object_container[k].position_current - 0.25 * delta * n
 
 
 # Runtime Settings
-dt = 1/60
+dt = ARGS.deltatime
 Running = True
+
+# experimental rendering offset
+ERO = 0.1
+if ARGS.use_experimental_rendering:
+    ERO = 1
 
 # Definitions
 objects = {}
@@ -219,8 +246,8 @@ while Running:
 
     DISPLAYSURF.fill(WHITE)
 
-    pygame.draw.circle(DISPLAYSURF, BLACK, (SCREEN_WIDTH/2,
-                       SCREEN_HEIGHT/2), SCREEN_HEIGHT/2.4)
+    draw_circle(DISPLAYSURF, SCREEN_WIDTH/2, SCREEN_HEIGHT/2,
+                SCREEN_HEIGHT/2.4, BLACK)
 
     if DEBUG:
 
@@ -235,8 +262,10 @@ while Running:
         dtCounter = FONT.render(f'DT: {round(dt, 5)}', True, BLACK, WHITE)
         DISPLAYSURF.blit(dtCounter, dtCounterRect)
 
-    cursor.rect.center = pygame.mouse.get_pos()
-    DISPLAYSURF.blit(cursor.image, cursor.rect)
+    # cursor (hollow circle)
+    draw_circle(DISPLAYSURF, *pygame.mouse.get_pos(), ARGS.radius, WHITE)
+    gfxdraw.aacircle(DISPLAYSURF, *pygame.mouse.get_pos(),
+                        ARGS.radius+1, BLACK)
 
     for obj in SOLVER.getInstances():
         obj.draw(DISPLAYSURF)
